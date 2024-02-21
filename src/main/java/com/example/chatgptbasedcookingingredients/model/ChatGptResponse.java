9621f743ae2e0826e39fd05add9e63d23554943d
Reebal Sami <reebal.sami@gmail.com>
2024-02-21 12:23:40 +0100

    ChatGptChoice ChatGptResponse added

---
+++ src/main/java/com/example/chatgptbasedcookingingredients/model/ChatGptResponse.java
@@ -0,0 +1,32 @@
+package com.example.chatgptbasedcookingingredients.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatGptResponse {
+
+    /*
+    {
+
+    "choices": [
+        {
+
+            "message": {
+                "role": "assistant",
+                "content": "This is a test!"
+            },
+
+        }
+    ],
+
+}
+     */
+
+    private List<ChatGptChoice> choices;
+
+}
