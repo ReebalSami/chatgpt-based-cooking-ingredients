3e1c04b0fd51a5ef8520d099020416dd3bc25ed9
Reebal Sami <reebal.sami@gmail.com>
2024-02-21 12:06:51 +0100

    ChatGptRequest added

---
+++ src/main/java/com/example/chatgptbasedcookingingredients/model/ChatGptRequest.java
@@ -0,0 +1,30 @@
+package com.example.chatgptbasedcookingingredients.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatGptRequest {
+
+    /*
+     * {
+     *      "model": "gpt-3.5-turbo",
+     *      "messages": [{"role": "user", "content": "Say this is a test!"}],
+
+     *    }
+     */
+
+    private String model;
+    private List<ChatGptMessage> messages;
+
+    public ChatGptRequest(String q){
+        this.model = "gpt-3.5-turbo";
+        this.messages = List.of(new ChatGptMessage(q));
+    }
+
+
+}
